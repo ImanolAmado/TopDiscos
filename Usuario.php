161394a5b5916357cd3f7191823ea8bd0477e9f4
aitana <ikaxbergareche23@lhusurbil.eus>
2024-10-11 13:49:55 +0200

    commit usuario

--- Usuario.php
+++ Usuario.php
@@ -1,9 +1,57 @@
 <?php
+Class Usuario{
+    private $id_usuario;
+    private $nombre;
+    private $password;
+    private $email;
+    private $rol;
+
+    function __construct($id_usuario, $nombre, $password, $email, $rol){
+        $this->id_usuario = $id_usuario;
+        $this->nombre = $nombre;
+        $this->password = $password;
+        $this->email = $email;
+        $this->rol = $rol;
+    }
+
+    //Getters y Setters
+
+    function getI_usuario(){
+        return $this->id_usuario;
+    }
+    function getNombre(){
+        return $this->nombre;
+    }
+    function getPassword(){
+        return $this->password;
+    }
+    function getEmail(){
+        return $this->email;
+    }
+    function getRol(){
+        return $this->rol;
+    }
+
+    function setId_usuario($id_usuario){
+        $this->id_usuario=$id_usuario;
+    }
+    function setNombre($nombre){
+        $this->nombre=$nombre;
+    }
+    function setPassword($password){
+        $this->password=$password;
+    }
+    function setEmail($email){
+        $this->email=$email;
+    }
+    function setRol($rol){
+        $this->rol=$rol;
+    }
 
 
-    echo 'hola!';
-    echo 'prueba';
-    echo 'prubea2';
+
+
+}
 
 
 ?>
